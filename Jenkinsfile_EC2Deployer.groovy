6c20128c4db17faef7b9be9e71cb27e81a78cbac
farrukh90 <farrukhsadykov@gmail.com>
2022-02-23 02:02:21 +0000

    updated branch

--- Jenkinsfile_EC2Deployer.groovy
+++ Jenkinsfile_EC2Deployer.groovy
@@ -1,5 +1,14 @@
 node {
-    
+    properties(
+	[buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+    parameters(
+		[string(defaultValue: 'apply', description: 'Should I build or destroy? ', name: 'TF_ACTION', trim: true),
+        choice(choices: 
+			[
+                'dev', 
+            ], 
+		description: 'Which Environment should we deploy?', 
+		name: 'ENVIR')])])
 	stage("Clone a Repo"){
         checkout([$class: 'GitSCM', branches: [[name: 'october2021']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/jenkins-class.git']]])
     }
