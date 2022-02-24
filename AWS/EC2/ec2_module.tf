7bc45d99c8eebbeade94b262671cff504d6ad492
NasibaNarbutaeva <nasiba_87@hotmail.com>
2022-02-24 23:28:28 +0000

    backend

--- AWS/EC2/ec2_module.tf
+++ AWS/EC2/ec2_module.tf
@@ -1,22 +1,22 @@
-data "aws_ami" "example" {
-  most_recent      = true
-  owners           = ["self"]
+// data "aws_ami" "example" {
+//   most_recent      = true
+//   owners           = ["self"]
 
-  filter {
-    name   = "name"
-    values = ["packer-centos7*"]
-  }
+//   filter {
+//     name   = "name"
+//     values = ["packer-centos7*"]
+//   }
 
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
+//   filter {
+//     name   = "root-device-type"
+//     values = ["ebs"]
+//   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
+//   filter {
+//     name   = "virtualization-type"
+//     values = ["hvm"]
+//   }
+// }
 
 
 resource "aws_instance" "web" {
